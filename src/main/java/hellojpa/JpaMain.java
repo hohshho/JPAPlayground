4f155e7430e78af9402ec22cd99a93701cf16654
hohshho <tkdgur8377@gmail.com>
2022-09-13 22:15:53 +0900

    init

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,40 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args){
+        // 인자로 persistence.xml에서 설정한 DB (persistence-unit)
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+
+            // 비영속
+            Member member = new Member();
+            member.setId(100L);
+            member.setName("HelloJPA");
+
+            // 영속
+            System.out.println("--- BEFORE ---");
+            em.persist(member);
+            System.out.println("--- AFTER ---");
+
+
+            tx.commit();
+        } catch (Exception e){
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+    }
+}
